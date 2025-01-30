5c5549db34f5b7d8ad8ef3264fd389649c33ee3d
JJ Pezor <jj.pezor@kellogg.northwestern.edu>
2025-01-30 01:47:38 +0000

    Updated practice and lab

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,43 @@
 
 # Arrays are lists of things
 
+favorite_foods = ["tacos","pizza","ice cream"]
+puts favorite_foods
+
+we_have_to_go_back = [4, 8, 15, 16, 23, 42]
+puts we_have_to_go_back
+
+shopping_lists = ["milk", "eggs", "toilet paper"], ["soap", "shampoo", "shaving cream"]
+puts shopping_lists
+p shopping_lists
+
 # Accessing the array
 
+favorite_foods = ["tacos","pizza","ice cream"]
+
+puts favorite_foods [-2]
+
+shopping_lists = ["milk", "eggs", "toilet paper"], ["soap", "shampoo", "shaving cream"]
+
+puts shopping_lists [1][0]
+
 # Add to the array
+
+# favorite_foods << "more tacos"
+# puts favorite_foods
+
+favorite_foods.push("more tacos")
+puts favorite_foods
+
+favorite_foods = favorite_foods + ["french fried", "ramen"]
+# puts favorite_foods
+p favorite_foods
+
+favorite_foods.pop
+puts favorite_foods
+
+puts "Favorite foods: #{favorite_foods.count}"
+puts "Shopping list items: #{shopping_lists.size}"
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
