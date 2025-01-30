5715f3e444a493481bd9f1d9c6856039d0403fc9
JJ Pezor <jj.pezor@kellogg.northwestern.edu>
2025-01-30 03:03:46 +0000

    Updated practices

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -4,8 +4,44 @@
 
 # Infinite Tacos -> Finite Tacos
 # loop do
-#   puts "tacos!"
+#    puts "tacos!"
+#  end
+
+# index = 0
+
+# loop do
+#     if index == 5
+#         break
+#     end
+#     puts "tacos!"
+
+#     index = index + 1
+
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+index = 0
+
+loop do
+    if index == tacos.size
+        break
+    end
+
+    taco = tacos[index]
+
+    puts "#{tacos} tacos!"
+
+    index = index + 1
+
+    if index == tacos.size
+        break
+    end
+end
+
+puts index
+
+for taco in tacos
+    puts taco
+end
